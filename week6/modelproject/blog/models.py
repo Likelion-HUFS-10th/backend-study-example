99c8f8dae8a31aab4f9903dcaae82e39af346883
430lyj <430lyj@gmail.com>
2022-07-04 22:38:07 +0900

    Week6 : init commit

---
+++ week6/modelproject/blog/models.py
@@ -0,0 +1,16 @@
+from distutils.command.upload import upload
+from django.db import models
+
+# Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    writer = models.CharField(max_length=10)
+    pub_date = models.DateTimeField()
+    image = models.ImageField(upload_to = 'blog/', blank = True, null = True)
+    body = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.body[:100]
---
+++ week6/modelproject/blog/views.py
@@ -0,0 +1,55 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
+
+from blog.forms import BlogForm
+from .models import *
+
+# Create your views here.
+def home(request):
+    blogs = Blog.objects.all()
+    return render(request, 'home.html', {'blogs':blogs})
+
+def detail(request, id):
+    # blog = Blog.objects.get(id = id)
+    blog = get_object_or_404(Blog, pk = id)
+    return render(request, 'detail.html', {'blog':blog})
+
+def new(request):
+    return render(request, 'new.html')
+
+def new_with_django_form(request):
+    form = BlogForm()
+    return render(request, 'new_with_django_form.html', {'form': form})
+
+def create_with_django_form(request):
+    form = BlogForm(request.POST, request.FILES) # form 데이터를 처리하기 위해서 request.POST와 request.FILES가 필요함을 의미합니다.
+    if form.is_valid(): # 유효성 검사 
+        new_blog = form.save(commit=False) # 임시 저장 나머지 필드(칼럼)를 채우기 위함
+        new_blog.pub_date = timezone.now()
+        new_blog.save()
+        return redirect('blog:detail', new_blog.id)
+    return redirect('home')
+
+def create(request):
+    new_blog = Blog()
+    new_blog.title = request.POST['title']
+    new_blog.writer = request.POST['writer']
+    new_blog.image = request.FILES['image']
+    new_blog.body = request.POST['body']
+    new_blog.pub_date = timezone.now()
+    new_blog.save()
+    return redirect('blog:detail', new_blog.id)
+
+def update(request, id):
+    blog = Blog.objects.get(id = id)
+    if request.method == "POST":
+        blog.title = request.POST["title"]
+        blog.body = request.POST["body"]
+        blog.save()
+        return redirect('blog:detail', blog.id)
+    return render(request, 'update.html', {'blog':blog})
+
+def delete(request, id):
+	blog = Blog.objects.get(id=id)
+	blog.delete()
+	return redirect("home")
