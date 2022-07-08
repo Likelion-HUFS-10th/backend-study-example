da166061c35c8e655bd2fcee44c67a45452b94e9
430lyj <430lyj@gmail.com>
2022-07-08 15:19:10 +0900

    Week4 : init commit

---
+++ week4/modelproject/blog/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from .views import *
+
+app_name = 'blog'
+
+urlpatterns = [
+    path('<int:id>', detail, name='detail'),
+    path('new', new, name='new'),
+    path('create', create, name='create'),
+    path('update/<int:id>', update, name='update'),
+    path('delete/<int:id>', delete, name='delete'),
+]
